f780e6a3935e8544da2cd6f6777394869dc05a46
aryamanm24 <aryamanmishra0234@gmail.com>
2026-03-25 14:12:55 -0400

    Time: 22 ms (63.89%), Space: 21 MB (85.09%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.py
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.py
@@ -1,23 +1,24 @@
 class Solution:
     def nextGreaterElements(self, nums: List[int]) -> List[int]:
         
+        # Duplicating the array twice
         length = len(nums)
+        new_arr = [0]*(2*length)
+        # [1, 2, 1] => [1, 2, 1, 1, 2, 1] # i + length
+        for i, num in enumerate(nums):
+            new_arr[i], new_arr[i+ length] = num, num
+        
+        # Created the double-length array, and now just perform monotonic stack
+        stack = [] # Store values
+        result = [-1]*length
 
-        circular = []
-        circular += nums
-        circular += nums
+        for i, num in enumerate(new_arr):
 
-        output = [-1]*length
-        stack = [] # will keep track of the indices
-
-        for i in range(2*length):
-            actual_i = i % length
-
-            while(stack and nums[stack[-1]] < nums[actual_i]):
-                output[stack[-1]] = nums[actual_i]
+            while(stack and nums[stack[-1]] < num):
+                result[stack[-1]] = num # Taking the circular arr in consideration
                 stack.pop()
             
-            if(actual_i < length):
-                stack.append(actual_i)
+            if(i < length):
+                stack.append(i)
         
-        return output
+        return result
