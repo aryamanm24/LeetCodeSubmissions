c5eb4043dcd1a5a0370f962c3fd44899a051fa35
aryamanm24 <aryamanmishra0234@gmail.com>
2026-03-28 11:27:51 -0400

    Time: 8 ms (42.3%), Space: 23.1 MB (35.02%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -3,21 +3,18 @@ class Solution:
         
         intervals.sort(key=lambda x:x[0])
 
-        # [[1, 4], [1, 5], [4, 5]]
-
-        result = [intervals[0]] # [1, 4]
+        # So the first element has the shortest start
+        # value at intervals[0][0]
+        result = [intervals[0]]
 
         for start, end in intervals[1:]:
 
-            # for i = 1, start = 1, end = 5 (from [1, 5])
-            last_end = result[-1][1] # get the last end from the last element of result = 4
-            if start <= last_end:
-                result[-1][1] = max(last_end, end) # try to grab the max between the ends
+            if(start <= result[-1][1] <= end):
+                result[-1][1] = end
+            elif(start <= end <= result[-1][1]):
+                continue
             else:
-                result.append([start, end]) # this works as we sorted the inner lists with the first element
+                interval = [start, end]
+                result.append(interval)
         
         return result
-
-
-
-
