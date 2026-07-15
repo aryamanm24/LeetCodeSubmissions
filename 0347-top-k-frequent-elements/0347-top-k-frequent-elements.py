3c64bedadcd0a33096bf63db674cdc3418556b24
aryamanm24 <aryamanmishra0234@gmail.com>
2026-07-15 12:05:26 -0400

    Time: 173 ms (5.13%), Space: 23 MB (41.88%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,34 +1,14 @@
-from collections import defaultdict
+from collections import Counter
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-
-        length = len(nums)
         
-        count_map = defaultdict(int)
+        number_count = Counter(nums)
 
-        for number in nums:
-            count_map[number] += 1
-        
         # {1: 4, 2: 4, 3: 2}
-
-        buckets = [[] for _ in range(length+1)]
-
-        # [[], [], [], [], [], [], [], [], [], [], []] -> since there are 10 elements in nums
-        # and then max frequency that a number can take is the length of nums
-
-        for num, freq in count_map.items():
-            buckets[freq].append(num)
-        
-        # [[], [], [3], [], [1, 2], [], [], [], [], [], []]
-
         result = []
 
-        for i in range(length, -1, -1):
-            for num in buckets[i]:
-                result.append(num)
-                
-                if(len(result) == k):
-                    return result
+        for i in range(k):
+            result.append(number_count.most_common()[i][0])
         
-        return []
+        return result
