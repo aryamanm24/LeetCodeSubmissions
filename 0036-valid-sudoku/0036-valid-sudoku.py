7661434de60a3a6da7698d7d6801d514afdceb38
aryamanm24 <aryamanmishra0234@gmail.com>
2026-07-21 01:57:24 -0400

    Time: 0 ms (100%), Space: 19.3 MB (34.36%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,26 +1,34 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        
+
+        # length of the board is fixed
         rows = [set() for _ in range(9)]
-        columns = [set() for _ in range(9)]
+        cols = [set() for _ in range(9)]
         boxes = [set() for _ in range(9)]
 
-        for r in range(9):
-            for c in range(9):
+        # rows = [(), (), (), (), (), (), (), (), ()]
+        # cols = [(), (), (), (), (), (), (), (), ()]
+        # boxes = [(), (), (), (), (), (), (), (), ()]
 
-                num = board[r][c]
+        # box logic -> box_row = row//3
+        # box_col = col//3
+        # box_index (in the list of sets) = (box_row)*3 + box_col
 
-                # Skip if empty
-                if(num == '.'):
-                    continue
-                
-                box_index = (r//3)*3 + (c//3)
-
-                if(num in rows[r] or num in columns[c] or num in boxes[box_index]):
-                    return False
-                
-                rows[r].add(num)
-                columns[c].add(num)
-                boxes[box_index].add(num)
-
+        for row in range(9):
+            for col in range(9):
+                value = board[row][col]
+                if(value != "."):
+                    box_row = row//3
+                    box_col = col//3
+                    box_index = (box_row*3) + box_col
+                    if(value in rows[row]):
+                        return False
+                    if(value in cols[col]):
+                        return False
+                    if(value in boxes[box_index]):
+                        return False
+                    rows[row].add(value)
+                    cols[col].add(value)
+                    boxes[box_index].add(value)
+        
         return True
