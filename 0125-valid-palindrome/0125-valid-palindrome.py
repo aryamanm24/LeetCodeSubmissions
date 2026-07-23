3a075190c8386e95526c34474778cc373759aa07
aryamanm24 <aryamanmishra0234@gmail.com>
2026-07-22 20:17:53 -0400

    Time: 7 ms (81.24%), Space: 19.4 MB (99.08%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,20 +1,20 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         
-        word_list = []
-
-        for char in s:
-            if(char.isalnum()):
-                word_list.append(char.lower())
-        
         left = 0
-        right = len(word_list) - 1
+        right = len(s) - 1
 
         while(left < right):
-            if(word_list[left] != word_list[right]):
+
+            while(left < right and not s[left].isalnum()):
+                left += 1
+            while(left < right and not s[right].isalnum()):
+                right -= 1
+            
+            if(s[left].lower() != s[right].lower()):
                 return False
             
             left += 1
             right -= 1
-        
+
         return True
