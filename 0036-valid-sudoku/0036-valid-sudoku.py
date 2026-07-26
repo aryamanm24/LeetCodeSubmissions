a8ee56dec1fe8f936316fb3fc8f0e77217dcea30
aryamanm24 <aryamanmishra0234@gmail.com>
2026-07-26 10:55:19 -0400

    Time: 7 ms (27.98%), Space: 19.5 MB (9.61%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,34 +1,30 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-
-        # length of the board is fixed
-        rows = [set() for _ in range(9)]
-        cols = [set() for _ in range(9)]
-        boxes = [set() for _ in range(9)]
-
-        # rows = [(), (), (), (), (), (), (), (), ()]
-        # cols = [(), (), (), (), (), (), (), (), ()]
-        # boxes = [(), (), (), (), (), (), (), (), ()]
-
-        # box logic -> box_row = row//3
-        # box_col = col//3
-        # box_index (in the list of sets) = (box_row)*3 + box_col
+        
+        rows_sets = [set() for _ in range(9)]
+        cols_sets = [set() for _ in range(9)]
+        boxes_sets = [set() for _ in range(9)]
 
         for row in range(9):
             for col in range(9):
+
                 value = board[row][col]
+
+                # computing box index:
+                box_row = row//3
+                box_col = col//3
+                box_index = (box_row*3) + box_col
+                # if cell is not empty, then check
                 if(value != "."):
-                    box_row = row//3
-                    box_col = col//3
-                    box_index = (box_row*3) + box_col
-                    if(value in rows[row]):
+                    if(value in rows_sets[row]):
                         return False
-                    if(value in cols[col]):
+                    if(value in cols_sets[col]):
                         return False
-                    if(value in boxes[box_index]):
+                    if(value in boxes_sets[box_index]):
                         return False
-                    rows[row].add(value)
-                    cols[col].add(value)
-                    boxes[box_index].add(value)
+                    
+                    rows_sets[row].add(value)
+                    cols_sets[col].add(value)
+                    boxes_sets[box_index].add(value)
         
         return True
