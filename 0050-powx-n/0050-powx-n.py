1b12f0e69a71e2ca7ac5c4c9b1fb308df58dc19a
aryamanm24 <aryamanmishra0234@gmail.com>
2026-08-10 02:48:11 -0400

    Time: 0 ms (100%), Space: 19.6 MB (19.88%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,4 +1,20 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
         
-        return (x**n)
+        # if power is less than 0
+        if(n < 0):
+            x = 1/x # set x = 1/x; 2^-2 => (1/2)^2
+            n = -n # in the above ex see how we transformed the power to be positive
+        
+        result = 1
+
+        while(n>0):
+
+            if(n%2 == 1):
+                # do the 'itself' multiplication once
+                result = result*x
+            
+            x = x*x
+            n = n//2
+        
+        return result
