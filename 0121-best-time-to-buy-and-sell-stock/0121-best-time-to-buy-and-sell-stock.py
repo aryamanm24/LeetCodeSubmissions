fe1f89b7f298922ff7bc778ef99958f89d953f33
aryamanm24 <aryamanmishra0234@gmail.com>
2026-08-10 02:33:33 -0400

    Time: 59 ms (33.06%), Space: 28.7 MB (40.81%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,15 +1,27 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+
+        if(not prices):
+            return 0
         
-        min_price = float('inf')
+        left = 0
+        right = 1
+
+        length = len(prices)
         max_profit = 0
 
-        for price in prices:
+        while(right < length):
 
-            if(price < min_price):
-                min_price = price
+            # if there's profit, then calculate max_profit so far
+            if(prices[right] > prices[left]):
+                curr_profit = prices[right] - prices[left]
+                max_profit = max(max_profit, curr_profit)
             
+            # else, you get a cheaper buy price, and since the sell day will be
+            # strictly after this buy date, we can say that there's potential for max_profit to increase
             else:
-                max_profit = max(max_profit, price - min_price)
+                left = right
+            
+            right += 1
         
         return max_profit
