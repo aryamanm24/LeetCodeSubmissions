150a5fa27dea63201fb791ff06d1d4b586004a3b
aryamanm24 <aryamanmishra0234@gmail.com>
2026-08-12 23:07:22 -0400

    Time: 2 ms (78.86%), Space: 19.4 MB (9.6%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,30 +1,33 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         
-        rows_sets = [set() for _ in range(9)]
-        cols_sets = [set() for _ in range(9)]
-        boxes_sets = [set() for _ in range(9)]
+        rows = [set() for _ in range(9)]
+        cols = [set() for _ in range(9)]
+        boxes = [set() for _ in range(9)]
+        # boxes = [(0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 2), (2, 0), (2, 1), (2, 2)]
 
         for row in range(9):
             for col in range(9):
 
                 value = board[row][col]
+                if(value != '.'):
 
-                # computing box index:
-                box_row = row//3
-                box_col = col//3
-                box_index = (box_row*3) + box_col
-                # if cell is not empty, then check
-                if(value != "."):
-                    if(value in rows_sets[row]):
-                        return False
-                    if(value in cols_sets[col]):
-                        return False
-                    if(value in boxes_sets[box_index]):
+                    box_row = row//3
+                    box_col = col//3
+
+                    box_index = (box_row * 3) + box_col
+
+                    if(value in rows[row]):
                         return False
                     
-                    rows_sets[row].add(value)
-                    cols_sets[col].add(value)
-                    boxes_sets[box_index].add(value)
+                    if(value in cols[col]):
+                        return False
+                    
+                    if(value in boxes[box_index]):
+                        return False
+                    
+                    rows[row].add(value)
+                    cols[col].add(value)
+                    boxes[box_index].add(value)
         
         return True
